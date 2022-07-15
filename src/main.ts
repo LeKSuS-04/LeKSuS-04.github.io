c2d10f5bdf843ccbb26621bdd983f88407171367
Alexey Tarasov <tarasovion2004@gmail.com>
2022-07-15 10:28:24 +0300

    Friendship ended with Vue, now Svelte is my best friend

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from "./router/index"
+import './app.css'
+import App from './App.svelte'
 
-createApp(App).use(router).mount('#app')
+const app = new App({
+  target: document.getElementById('app'),
+  intro: true,
+})
+
+export default app
